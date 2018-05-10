cd89587403c1b9dbdbd30708fb198ce0a6ede9f2
Kevin <stoutkev3@gmail.com>
2018-05-10 10:31:22 -0400

    added about section and skills section

--- js/main.js
+++ js/main.js
@@ -6,4 +6,32 @@ $(document).ready(function(){
         pagination: false
     });
 
+    var typed = new Typed(".typed", {
+        strings: ["Software Engineer.", "Web Developer." , "Programmer."],
+        typeSpeed: 70,
+        loop: true,
+        startDelay: 1000,
+        showCursor: false
+    });
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        autoplay: true,
+        autoplayTimeout: 1000,
+        autoplayHoverPause: true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+
 });
